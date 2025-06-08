d0ea85bbda64dcd8ad567b4abacc93c97439877a
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-06-08 12:27:51 -0400

    init routine to manually install first actor.

---
+++ src/common/init.c
@@ -0,0 +1,18 @@
+#include "init.h"
+#include "actor.h"
+#include "logo/joypad/joypad.h"
+#include "common/scene.h"
+
+void init(void)
+{
+	Actor *a = ACTORHEAP + sizeof(ACTORHEAP)/sizeof(*ACTORHEAP);
+	do (--a)->process_flag = 0xFF;
+	while (a != ACTORHEAP);
+
+	a->process_flag = 1;
+	a->process_func = readJoypad;
+	a->bank = 0;
+	a->next = NULL;
+
+	switchScene(0);
+}
