6503c6c41c7fcc2befdd62a15940d302df21e866
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-06-08 12:25:47 -0400

    create actor struct and helper routines to manage linked list.

---
+++ src/common/actor.c
@@ -0,0 +1,61 @@
+#include "actor.h"
+#include "common.h"
+#include <string.h>
+#include <gb/gb.h>
+
+Actor ACTORHEAP[32];
+Actor *next_free = ACTORHEAP + 1;
+
+void spawnActor(void (*actor_func)(Actor*), u8 bankID, u16 actor_var)
+{
+	//copy actor data into the list
+	next_free->process_flag = 1;
+	next_free->bank = bankID;
+	next_free->process_func = actor_func;
+	next_free->variable = actor_var;
+	#if DEBUG
+		memset(next_free->_actor_memory, 0, sizeof(next_free->_actor_memory));
+	#endif
+	next_free->next = NULL;
+
+	//find end of list and append new actor
+	Actor *a = ACTORHEAP;
+	while (a->next)
+		a = a->next;
+	a->next = next_free;
+	
+	//find empty slot in heap somewhere
+	a = ACTORHEAP;
+	do ++a;
+	while(a->process_flag != 0xFF);
+	next_free = a;
+}
+
+void spawnActorH(ActorHeader *a)
+{
+	spawnActor(a->process_func, a->bank, a->variable);
+}
+
+void removeActor(Actor *target)
+{
+	Actor *a = ACTORHEAP;
+	while (a->next != target)
+		a = a->next;
+	a->next = target->next;
+
+	target->process_flag = 0xFF;
+}
+
+void runAllActors(void)
+{
+	Actor *a = ACTORHEAP;
+	do
+	{
+		if (a->process_flag)
+		{
+			SWITCH_ROM(a->bank);
+			a->process_func(a);
+		}
+		a = a->next;
+	} while (a);
+}
---
+++ src/common/actor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "common.h"
+#include <gb/gb.h>
+
+typedef struct actor_t
+{
+	u8 process_flag; //1
+	void (*process_func)(struct actor_t*); //3
+	u8 bank; //4
+	u16 variable; //6
+	u8 _actor_memory[24]; //30
+	struct actor_t *next; //32
+} Actor;
+
+typedef struct actor_header_t
+{
+	void (*process_func)(struct actor_t *);
+	u8 bank;
+	u16 variable;
+} ActorHeader;
+
+extern Actor ACTORHEAP[32];
+extern Actor *next_free;
+
+void spawnActor(void (*)(Actor*), u8, u16);
+void spawnActorH(ActorHeader *);
+void removeActor(Actor *);
+void runAllActors(void);
