de2b63886bc62f7c6ecd7130f0becf73a97918af
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-06-21 15:57:27 -0400

    add new banked gfx loader to the logo scene

--- src/logo/scenetable.h
+++ src/logo/scenetable.h
@@ -3,6 +3,6 @@
 #include "common/fade.h" 
 
 const ActorHeader logo[] = {
-	{loadBKG, 0, 0, NULL},
+	{loadBKG, BANK(loadBKG_bank), 0, NULL},
 	{fadeScreen, BANK(fadeScreen_bank), 0x0060, colorActor}
 };
