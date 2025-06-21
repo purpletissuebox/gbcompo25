4fe1c12eb278979b760a4dced22c3513f85cdc01
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-06-21 15:50:47 -0400

    move gfx tiles to their own folder

--- res/hello.c
+++
@@ -1,6 +0,0 @@
-#include "hello.h"
-#include <gb/cgb.h>
-
-INCBIN(helloworld_tiledata, "res/tiles.bin")
-INCBIN(helloworld_tilemap, "res/map.bin")
-INCBIN(helloworld_attrmap, "res/attr.bin")
--- res/hello.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <gbdk/incbin.h>
-#include <gb/cgb.h>
-
-INCBIN_EXTERN(helloworld_tiledata)
-INCBIN_EXTERN(helloworld_attrmap)
-INCBIN_EXTERN(helloworld_tilemap)
