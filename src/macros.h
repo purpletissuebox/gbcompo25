24fc7586cdd2d029ca84071298caea64800788be
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-06-25 23:03:49 -0400

    consolidate common macros to their own file

---
+++ src/macros.h
@@ -0,0 +1,35 @@
+MACRO ldsz
+	ld \1, (\2.end - \2)
+ENDM
+
+MACRO lddouble
+	ld \1, ((\2) << 8) | (\3)
+ENDM
+
+MACRO swapInRom
+	ldh a, [rom_bank]
+	push af
+	ld a, BANK(\1)
+	ldh [rom_bank], a
+	ld [MBC_ROM_BANK], a
+ENDM
+
+MACRO swapInRam
+	ldh a, [ram_bank]
+	push af
+	ld a, BANK(\1)
+	ldh [ram_bank], a
+	ldh [IO_WRAM_BANK], a
+ENDM
+
+MACRO restoreBankRom
+	pop af
+	ldh [rom_bank], a
+	ld [MBC_ROM_BANK], a
+ENDM
+
+MACRO restoreBankRam
+	pop af
+	ldh [ram_bank], a
+	ldh [IO_WRAM_BANK], a
+ENDM
