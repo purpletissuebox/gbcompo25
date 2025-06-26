619b28778ed59b1ccafdab7e7a94d4630a65528b
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-06-26 00:05:45 -0400

    add optional arguments for newactor macro

--- src/common/actor.h
+++ src/common/actor.h
@@ -2,10 +2,26 @@ EXPORT DEF ACTORSIZE EQU $0020
 
 ;init, process, variable
 MACRO NEWACTOR
-	db (\3)
-	db BANK(\2)
-	dw (\2)
-	dw (\1)
-	db BANK(\1)
-	db $00
+	IF _NARG == 3
+		db (\3)
+		db BANK(\2)
+		dw (\2)
+		dw (\1)
+		db BANK(\1)
+		db $FF
+	ELIF _NARG == 2
+		db (\2)
+		db BANK(\1)
+		dw (\1)
+		dw $0000
+		db $00
+		db $FF
+	ELSE
+		db $00
+		db BANK(\1)
+		dw (\1)
+		dw $0000
+		db $00
+		db $FF
+	ENDC
 ENDM
