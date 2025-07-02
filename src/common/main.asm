ed68cc73b812e57dcc2287ed72600eb780f1943e
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-07-01 23:01:59 -0400

    basic main loop to tick the fade

--- src/common/main.asm
+++ src/common/main.asm
@@ -1,5 +1,30 @@
+INCLUDE "hwregs.h"
+INCLUDE "common/actor.h"
+
 SECTION "MAIN", ROM0
 MAIN::
-    .loop:
-        halt 
-    jr MAIN.loop
+	ld hl, actor_heap + ACTORSIZE - 5
+	.loop:
+		ldi a, [hl]
+		ldh [rom_bank], a
+		ld [MBC_ROM_BANK], a
+		ldi a, [hl]
+		ld e, a
+		ldi a, [hl]
+		push hl
+		ld b, h
+		ld h, a
+		ld a, l
+		ld l, e
+		and $E0
+		ld c, a
+		rst callHL
+		pop hl
+		ldi a, [hl]
+		or [hl]
+		ldd a, [hl]
+		ld l, [hl]
+		ld h, a
+	jr nz, .loop
+	halt
+    jr MAIN
