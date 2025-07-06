bff7acb3fd0926cedd973a1f3fe0255f4ec9a1cd
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-07-06 18:14:15 -0400

    check fadeout before starting timer

--- src/logo/stall.asm
+++ src/logo/stall.asm
@@ -1,11 +1,23 @@
 INCLUDE "common/actor.h"
+INCLUDE "macros.h"
 
 SECTION "LOGO STALL", ROMX
 logoStall::
+	swapInRam fade_complete
+	ld a, [fade_complete]
+	ld e, a
+	restoreBankRam
+	ld a, e
+	and a
+	ret z
+	updateActorMain logoTick
+	ret
+
+logoTick:
 	ld hl, VARIABLE
 	add hl, bc
 	dec [hl]
-	ret z
+	ret nz
 	
 	push bc
 	ld de, .fadeout_actor
